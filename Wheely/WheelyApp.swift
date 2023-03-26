385cadc216abb06118d85b9de33d81e821c28dc8
Ants Gardiner <antokne@gmail.com>
2023-03-26 14:17:03 +1300

    Initial Commit

---
+++ Wheely/WheelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelyApp.swift
+//  Wheely
+//
+//  Created by Antony Gardiner on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WheelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
